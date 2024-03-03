30c05de3f348d3d1e17dac5f4bc64fbf2c7a80ec
CallumLee22 <callumlee.2822@gmail.com>
2024-03-03 00:10:09 +0000

    navbar

--- app.py
+++ app.py
@@ -18,5 +18,17 @@ app.config["SECRET_KEY"] = "secret_key"
 def index():
     return render_template("index.jinja")
 
+@app.route("/users")
+def users():
+    return render_template("users.jinja")
+
+@app.route("/groups")
+def groups():
+    return render_template("groups.jinja")
+
+@app.route("/departments")
+def departments():
+    return render_template("departments.jinja")
+
 if __name__ == "__main__":
     app.run()
