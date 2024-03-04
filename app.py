51987b5668ca8633441a0ea3c87ffcffc3637178
Callum Lee <callum.lee@stfc.ac.uk>
2024-03-04 11:14:35 +0000

    display users' names

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 from dotenv import load_dotenv
-from supabase import create_client, Client
+from supabase import create_client
 from flask import Flask, render_template
-import numpy
 
 load_dotenv()
 
@@ -15,39 +14,54 @@ app = Flask(__name__)
 app.app_context().push()
 app.config["SECRET_KEY"] = "secret_key"
 
+
 @app.route("/")
 def index():
     return render_template("index.jinja")
 
+
 @app.route("/users")
 def users():
-    all_users = supabase.table("Users").select("*").execute().data
-    full_names = []
-    
-    for names in all_users:
-       full_names.append(names["first_name"] + " " + names["last_name"])
+    all_users = (
+        supabase.table("Users")
+        .select("user_id", "first_name", "last_name")
+        .execute()
+        .data
+    )
+    user_names = []
 
-    return render_template("users.jinja", full_names=full_names)
+    for name in all_users:
+        user_names.append(
+            {
+                "user_id": name["user_id"],
+                "full_name": name["first_name"] + " " + name["last_name"],
+            }
+        )
+
+    return render_template("users.jinja", user_names=user_names)
+
 
 @app.route("/groups")
 def groups():
     all_groups = supabase.table("Groups").select("*").execute().data
     names = []
-    
+
     for name in all_groups:
-       names.append(name["name"])
-    
+        names.append(name["name"])
+
     return render_template("groups.jinja", names=names)
+
 
 @app.route("/departments")
 def departments():
     all_users = supabase.table("Departments").select("*").execute().data
     names = []
-    
+
     for name in all_users:
-       names.append(name["name"])
-    
+        names.append(name["name"])
+
     return render_template("departments.jinja", names=names)
+
 
 if __name__ == "__main__":
     app.run()
